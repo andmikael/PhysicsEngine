2bb757cf4c3e9776cf9f20b4fe3195394573424b
Mikael Andersson <mikael.andersson@tuni.fi>
2023-12-06 22:13:08 +0200

    feat: added window

--- PhysicsEngine.pro
+++ PhysicsEngine.pro
@@ -4,4 +4,18 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
-        main.cpp
+        circleobject.cpp \
+        main.cpp \
+        renderer.cpp
+
+INCLUDEPATH += D:\ohjelmat\repos\personal\cpp\sfml\SFML-2.6.1-sources\SFML-2.6.1\include
+DEPENDPATH += D:\ohjelmat\repos\personal\cpp\sfml\SFML-2.6.1-sources\SFML-2.6.1\include
+
+LIBS += -LD:\ohjelmat\repos\personal\cpp\sfml\sfml_build\libs\
+
+CONFIG(debug, debug|release): LIBS += -lsfml-audio-d -lsfml-graphics-d -lsfml-main-d -lsfml-network-d -lsfml-system-d -lsfml-window-d
+CONFIG(release, debug|release): LIBS += -lsfml-audio -lsfml-graphics -lsfml-main -lsfml-network -lsfml-system -lsfml-window
+
+HEADERS += \
+    circleobject.h \
+    renderer.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    constexpr int32_t width = 1280;
+    constexpr int32_t height = 720;
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 1;
+
+    sf::RenderWindow window(sf::VideoMode(width, height), "PhysicsEngine", sf::Style::Default, settings);
+
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
     return 0;
 }
